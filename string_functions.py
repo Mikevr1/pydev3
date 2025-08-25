84078fbf8f5e22860fb5acbf74b94faba0833806
Mike Van Rooyen <mikevr6161@gmail.com>
2025-08-25 18:36:34 +0800

    first commit

---
+++ string_functions.py
@@ -0,0 +1,27 @@
+#######################################################
+#name: string_fuctions.py
+#description:various string functions tafe s2 exercises
+#author:mikevr
+#tafe ID:20154897
+#date:24/08/2025
+#######################################################
+
+#1 Ask the user for a word and print its length
+
+#nput_word =input('enter a word ')
+#print (f"the lenght is {len(input_word)}")
+
+#######################################################
+
+
+#2 Ask the user for a sentence and print it with the first character capitalized
+
+input_sentence=input('enter a sentence ')
+print(f"{input_sentence.capitalize()}")
+
+######################################################
+
+#2B In what situations is this a bad idea?
+sentence = "Old MacDonald had a farm"
+print( sentence.lower())#when using lower
+print( sentence.upper())#when using upper
